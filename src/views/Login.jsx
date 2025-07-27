753f4601285e3e24a3ed8482fcc7208c5ce7a78b
AstraAbigail <astrabigail@gmail.com>
2025-07-27 20:34:19 -0300

    NF:add link to home - Login: add hook form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,12 +1,84 @@
 import { Layout } from "../components/Layout.jsx"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
 
+const Login = () => {
+  
+  const [success, setSuccess] = useState("")
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm({
+    //valores por defecto de los input
+    defaultValues: {
+      email: (""),
+      password: ("")
+    }
+  })
 
-const Login = () => { 
+  const isSubmit = (data) => {
+       
+    console.log(data, ("<- Usuario"))
+    //Mesnaje de exito
+    setSuccess("Ingresando...")
+
+    //limpio los input, volviendolos a su valor por defecto.
+    reset()
+    //delay
+    /*setTimeout(() => {
+      setSuccess("")
+    }, 5000); // 5000 ms = 5 segundos
+    */    
+  }
+
   return (    
-    <>
-      
+    <>      
       <Layout>
-        <h1>Login!</h1>
+        <section>        
+          <form onSubmit={handleSubmit(isSubmit)}>
+            <div>
+              <input
+                type="email"
+                placeholder= "Correo"
+                {...register("email",
+                  { required: {
+                      value: true,
+                      message: "Este campo es requerido",
+                    },
+                    pattern:{
+                      value:/^[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com(\.ar)?|yahoo\.com(\.ar)?)$/,
+                      message:"Dominios admitidos:@hotmail.com / @hotmail.com.ar / @gmail.com / @yahoo.com / @yahoo.com.ar",
+                    }                  
+                  }
+                )}
+              />
+            </div>
+            <p>{errors.email?.message}</p>
+            <div>
+              <input
+                type="password"
+                placeholder="Contraseña"             
+                {...register("password",
+                  {
+                    required: {
+                      value: true,
+                      message: "Este campo es requerido",
+                    },
+                    pattern: {
+                      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+                      message: "Debe contener: 1 Carácter especial - Minúsculas - Números - 1 Mayúscula - 8 Carácteres",
+                    }
+                  }
+                )}
+              />
+            </div>
+            <p>{errors.password?.message}</p>
+            <button>Ingresar</button>
+          </form>            
+          {success && <p style={{ color: "green" }}>{success}</p>}      
+        </section>
       </Layout>
       
     </>
@@ -14,5 +86,6 @@ const Login = () => {
 
 
 }
+
 
 export {Login }
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,9 +1,11 @@
 import { Layout } from "../components/Layout"
+import { Link } from "react-router-dom"
 
 const NotFound = () => { 
   return (
     <Layout>
-     <h1>NotFound</h1>
+      <h1>Página no encontrada - 404</h1>
+      <p>Volver a la pagina de inicio: <Link to="/">Inicio</Link></p>
     </Layout>
    
 )
