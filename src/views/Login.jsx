55418bdd57065ba317c72303a6fbacaff307b2b1
AstraAbigail <astrabigail@gmail.com>
2025-08-07 22:51:40 -0300

    add:index font - add:style al login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { userAuth } from "../context/UserContext.jsx"
 import { useNavigate } from "react-router-dom"
+import "../styles/views/Login.css" 
 
 const Login = () => {
   
@@ -31,12 +32,15 @@ const Login = () => {
 
     if (isLogin) {
       console.log(data, ("<- Usuario"))
-      //Mesnaje de exito
-      setSuccess("Ingresando...")
+      setSuccess("...ingresando");
       //limpio los input, volviendolos a su valor por defecto.
       reset()
-      //redirijo automaticamente al Home
-      navigate("/")
+
+      setTimeout(() => {
+        //redirijo automaticamente al Home
+        navigate("/") 
+      }, 3000); 
+      
     }
     
     
@@ -46,33 +50,34 @@ const Login = () => {
   return (    
     <>      
       <Layout>
-        <section>        
-          <form onSubmit={handleSubmit(isSubmit)}>
-            <div>
-              <input
-                type="text"
-                placeholder="Usuario"
-                {...register("username",
-                  { required: {
-                      value: true,
-                      message: "Este campo es requerido"
-                  },
-                    minLength: {
-                      value: 4,
-                      message:"Debe tener 4 carácteres como mínimo."
+        <section className="section-login-layout">        
+          <form className="section-login-layout-form" onSubmit={handleSubmit(isSubmit)}>
+              <h3>Iniciar Sesion</h3>
+              <div className="section-login-layout-form-username">
+                <input
+                  type="text"
+                  placeholder="Usuario"
+                  {...register("username",
+                    { required: {
+                        value: true,
+                        message: "*Este campo es requerido"
                     },
-                    pattern: {
-                      //al menos minuscula, numero,caracter especial, esten todos los anteriores
-                      // value:/^(?=.*[a-z])(?=.*\d)(?=.*[_\-@%$])[a-z\d_\-@%$]+$/,
-                      value:/^[a-z]+$/,
-                      message:"Debe contener al 4 letras minusculas"
-                    }                  
-                  }
-                )}
-              />
-            </div>
-            <p style={{color:"red"}}>{errors.username?.message}</p>
-            <div>
+                      minLength: {
+                        value: 4,
+                        message:"Debe tener 4 carácteres como mínimo."
+                      },
+                      pattern: {
+                        //al menos minuscula, numero,caracter especial, esten todos los anteriores
+                        // value:/^(?=.*[a-z])(?=.*\d)(?=.*[_\-@%$])[a-z\d_\-@%$]+$/,
+                        value:/^[a-z]+$/,
+                        message:"Debe contener al 4 letras minusculas"
+                      }                  
+                    }
+                  )}
+                />
+              </div>
+            <p style={{color:"white"}}>{errors.username?.message}</p>
+            <div className="section-login-layout-form-password">
               <input
                 type="password"
                 placeholder="Password"             
@@ -80,7 +85,7 @@ const Login = () => {
                   {
                     required: {
                       value: true,
-                      message: "Este campo es requerido"
+                      message: "*Este campo es requerido"
                     },
                     minLength: {
                       value: 6,
@@ -95,10 +100,11 @@ const Login = () => {
                 )}
               />
             </div>
-            <p style={{color:"red"}}>{errors.password?.message}</p>
+            <p style={{color:"white"}}>{errors.password?.message}</p>
             <button>Ingresar</button>
+            {success && <p style={{ color: "white" }}>{success}</p>} 
           </form>            
-          {success && <p style={{ color: "green" }}>{success}</p>}      
+            
         </section>
       </Layout>
       
